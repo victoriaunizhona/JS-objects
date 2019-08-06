c441d3a71f8ffeb5858934ef48773b3b6e8a7409
victoria.unizhona <vikaunizhona@gmail.com>
2019-08-06 08:47:35 +0300

    added constructor and this practice

---
+++ app.js
@@ -0,0 +1,31 @@
+//JavaScript Object Literal. A JavaScript object literal is a comma-separated list of name-value pairs wrapped in curly braces.
+
+// Object literal
+var myObject = {
+  sProp: 'some string value',
+  numProp: 2,
+  bProp: false
+};
+
+// Person constructor
+
+function Person(name, dob) {
+  this.name = name;    // refers to the current instance of person
+  this.birthday = new Date(dob); //full text string:
+  this.calculateAge = function () {
+    const diff = Date.now() - this.birthday.getTime(); //The Date.now() method returns the number of milliseconds elapsed since January 1, 1970 00:00:00 UTC.
+    // The getTime() method returns the number of milliseconds
+
+
+    const ageDate = new Date(diff);
+    console.log(ageDate.getUTCFullYear());
+    return ageDate.getUTCFullYear() - 1970
+
+
+  }
+} //Should start with a capital
+
+const brad = new Person("Brad", "9-10-1911");
+
+console.log(brad.calculateAge());
+
